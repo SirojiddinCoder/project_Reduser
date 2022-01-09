a9da5827fbea0f6d88491f599545b60511703175
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2022-01-09 23:35:12 +0500

    test github

---
+++ my-app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    {/* <App /> */}
+    <h1>React Js Next Js</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
